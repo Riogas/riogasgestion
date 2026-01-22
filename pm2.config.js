77b642622faa6d5cc91f2e99281f5edaff8e0521
computos <computos.riogas@gmail.com>
2026-01-22 14:39:11 -0300

    feat: configurar PM2 en modo fork para mejor rendimiento con Next.js

--- pm2.config.js
+++ pm2.config.js
@@ -4,6 +4,7 @@ module.exports = {
       name: 'goya',
       script: 'node_modules/next/dist/bin/next',
       args: 'start',
+      exec_mode: 'fork',
       instances: 1,
       autorestart: true,
       watch: false,
